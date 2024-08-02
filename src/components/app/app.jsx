041be9cbd67a1fd61794d1ec46935f79d26a9735
Mirror <yllikano@gmail.com>
2024-08-02 12:53:01 +0300

    тема и иконки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import features from "../../mocks/features";
+import { GlobalStyle } from "./styled";
+
 export default function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyle/>
       Test
-    </div>
+    </>
   );
 }
