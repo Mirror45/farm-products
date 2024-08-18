41c41d60b3263395ef42c1daabf5a0640fd3aeae
Mirror <yllikano@gmail.com>
2024-08-18 13:23:24 +0300

    app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import PageWrapper from "../layout/page-wrapper/page-prapper";
+import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
 import features from "/src/mocks/features";
 import products from "/src/mocks/products";
 import { GlobalStyle } from "./styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRoute } from "/src/const";
 
-export default function App() {
+// Корневой компонент всего приложения
+function App() {
   return (
     <>
       <GlobalStyle />
-      <PageWrapper features={features} products={products}>Контент страницы</PageWrapper>
+      <Router>
+        <Routes>
+          <Route exact path={AppRoute.MAIN}>
+            <Route
+              index
+              element={<PageWrapper features={features} products={products} />}
+            />
+            <Route
+              exact
+              path={AppRoute.ORDER}
+              element={<PageWrapper features={features} products={products} />}
+            />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
+
+export default App;
